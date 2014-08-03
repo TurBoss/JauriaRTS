04549ac46cc70319fb08969c15fc7f098dfdf76d
PerroTron <perrotronn@gmail.com>
2014-08-03 04:53:47 +0200

    Nuevo Modelo DT-6
    Eliminado ST-6 (volador)

---
+++ Weapons/weapondt6.lua
@@ -0,0 +1,52 @@
+
+local weaponName = "weapondt6"
+
+local weaponDef = {
+	
+	name                    = "metralletaNM-1",
+	weaponType              = [[Cannon]],
+	
+	Accuracy=100,
+	movingAccuracy=200,
+	
+	--damage
+	
+	avoidFeature = false,
+	
+	damage = {
+		default = 15,
+		heavyarmor = 3,
+		lightarmor = 18,
+	},
+	areaOfEffect            = 80,
+	
+	--physics
+	
+	weaponVelocity          = 700,
+	reloadtime              = 0.8,
+	range                   = 500,
+--	sprayAngle              = 45,
+	weaponaceleration       = 80,
+	tolerance               = 10000,
+	lineOfSight             = true,
+	turret                  = true,
+	craterMult              = 0,
+--	burst                   = 2,
+--	burstrate               = 0.08,
+	--proyectiles             = 2,
+	
+	--apperance
+	
+	rgbColor                = [[1 0 0.2]],
+	size                    = 8,
+	stages                  = 8,
+	intensity               = 10,
+	separation              = 1.5,
+	
+	--sounds
+	
+	soundStart              = "disparo_gfx",
+	
+	collideFriendly=true,
+}
+return lowerkeys({[weaponName] = weaponDef})
