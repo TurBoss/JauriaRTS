261d3a239da1f2ab2270547de7e16638e300be51
TurBoss <j.l.toledano.l@gmail.com>
2014-08-13 00:09:23 +0200

    LUPS

--- LuaRules/Utilities/tablefunctions.lua
+++ LuaRules/Utilities/tablefunctions.lua
@@ -2,15 +2,15 @@
 -------------------------------------------------------------------------------------
 --deep not safe with circular tables! defaults To false
 function Spring.Utilities.CopyTable(tableToCopy, deep)
-  local copy = {}
-  for key, value in pairs(tableToCopy) do
-    if (deep and type(value) == "table") then
-      copy[key] = Spring.Utilities.CopyTable(value, true)
-    else
-      copy[key] = value
-    end
-  end
-  return copy
+	local copy = {}
+	for key, value in pairs(tableToCopy) do
+		if (deep and type(value) == "table") then
+			copy[key] = Spring.Utilities.CopyTable(value, true)
+		else
+			copy[key] = value
+		end
+	end
+	return copy
 end
 
 -- modifies primary directly; if you don't want to do that then make a copy and pass that
