452ed997a076b3814a963de425cfee5ea3905f03
PerroTron <perrotronn@gmail.com>
2014-08-24 18:48:08 +0200

    New unit Haerik

---
+++ Units/haerik.lua
@@ -0,0 +1,59 @@
+local unitName = "haerik"
+
+local unitDef = {
+	name = "haerik",
+	Description = "Complex, mobile unit.",
+	objectName = "haerik.dae",
+	script = "haerik.lua",
+	buildPic = "placeholder.png",
+	iconType = "rk2",
+
+	--cost
+
+	buildCostMetal = 1380,
+	buildCostEnergy = 0,
+	buildTime = 12,
+
+	--Health
+
+	maxDamage = 3200,
+	idleAutoHeal = 10,
+	idleTime     = 3,
+
+	--Movement
+
+	mass=1000,
+	Acceleration = 2,
+	BrakeRate = 1.5,
+	FootprintX = 2,
+	FootprintZ = 2,
+	MaxSlope = 12,
+	MaxVelocity = 4,
+	MaxWaterDepth = 20,
+	MovementClass = "Default2x2",
+	TurnRate = 2600,
+
+	sightDistance = 400,
+
+	Category = [[LAND]],
+
+	CanAttack = true,
+	CanGuard = true,
+	CanMove = true,
+	CanPatrol = true,
+	CanStop = true,
+	LeaveTracks = false, 
+
+
+	weapons = {
+		[1]={name  = "weaponhaerik",
+			maxAngleDif = 90,
+			mainDir = [[0 0 1]],
+			onlyTargetCategory = [[LAND]],
+		},
+		[2]={name  = "shieldhaek",
+		},
+	},
+}
+
+return lowerkeys({ [unitName] = unitDef })
---
+++ Weapons/weaponhaerik.lua
@@ -0,0 +1,55 @@
+
+local weaponName = "weaponhaerik"
+
+local weaponDef = {
+	
+	name                    = "metralletaNM-1",
+	weaponType              = "LaserCannon",
+	
+	Accuracy=50,
+	movingAccuracy=80,
+	
+	--damage
+	
+	avoidFeature = false,
+	
+	damage = {
+		default = 200,
+		heavyarmor = 5,
+		lightarmor = 120,
+	},
+	areaOfEffect            = 40,
+	
+	--physics
+	
+	weaponVelocity          = 800,
+	reloadtime              = 0.3,
+	range                   = 480,
+--	sprayAngle              = 45,
+	weaponaceleration       = 80,
+	tolerance               = 10000,
+	lineOfSight             = true,
+	turret                  = true,
+	craterMult              = 0,
+--	burst                   = 2,
+--	burstrate               = 0.08,
+	--proyectiles             = 2,
+	
+	--apperance
+	
+	rgbColor2                = [[0 0.1 1]],
+	size                    = 5,
+	stages                  = 8,
+	intensity               = 10,
+	separation              = 1.6,
+	explosionScar			= true,
+	
+	--explosionGenerator		= "custom:humo",
+	--explosiongenerator			= [[custom:genericshellexplosion-small]],
+	--sounds
+	
+	soundStart              = "disparo_gfx",
+	
+	collideFriendly=true,
+}
+return lowerkeys({[weaponName] = weaponDef})
