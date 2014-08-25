5de163fc9e8ff243d8eebe319dc9066a7a6ff79e
TurBoss <j.l.toledano.l@gmail.com>
2014-08-25 08:24:45 +0200

    New AI and 2 example AI

--- LuaRules/Gadgets/JauriaAI.lua
+++ LuaRules/Gadgets/JauriaAI.lua
@@ -1,16 +1,58 @@
+-- In-game, type /luarules ai in the console to toggle the ai debug messages
 function gadget:GetInfo()
     return {
         name    = "JauriaAI",
         desc    = "An AI that knows how to play Mod X",
-        author  = "John Doe",
-        date    = "2020-12-31",
-        license = "Public Domain",
+        author  = "TurBoss",
+        date    = "2014-08-25",
+        license = "GNU GPL ver 2 or later",
+        version = "0.1",
         layer   = 82,
         enabled = true
     }
 end
 
-function gadget:GameStart() 
+local AI_Debug_Mode = 0 -- Must be 0 or 1
+
+local function ChangeAIDebugVerbosity(cmd,line,words,player)
+	local lvl=tonumber(words[1])
+	if lvl then
+		AI_Debug_Mode=lvl
+		Spring.Echo("AI debug verbosity set to "..AI_Debug_Mode)
+	else
+		if AI_Debug_Mode>0 then
+			AI_Debug_Mode=0
+		else
+			AI_Debug_Mode=1
+		end
+		Spring.Echo("AI debug verbosity toggled to "..AI_Debug_Mode)
+	end
+	return true
+end
+
+local function AIDebugMessage(t,message)
+	if(AI_Debug_Mode>0) then
+		Spring.Echo("Team["..t.."] "..message)
+	end
+end
+
+local function SetupCmdChangeAIDebugVerbosity()
+	local cmd,func,help
+	cmd  = "ai"
+	func = ChangeAIDebugVerbosity
+	help = " [0|1]: make the LUA AI shut up or fill your infolog"
+	gadgetHandler:AddChatAction(cmd,func,help)
+	Script.AddActionFallback(cmd..' ',help)
+end
+
+function gadget:GameFrame(frame)
+	if (frame % 30 ==0) then
+		AIDebugMessage("all","testMSG EVERY 30 Frames".. frame)
+	end
+end
+
+function gadget:GameStart()
+	SetupCmdChangeAIDebugVerbosity()
     -- Initialise AI for all teams that are set to use it
     for _,t in ipairs(Spring.GetTeamList()) do
         local _,_,_,isAI,side = Spring.GetTeamInfo(t)
@@ -20,4 +62,8 @@ function gadget:GameStart()
             local home_x,home_y,home_z = Spring.GetTeamStartPosition(t)
         end
     end
+	Spring.GiveOrderToUnit(1000,-1,{0,0,0,0},{}) -- uid, id {pos x, pos y, pos z, dir}
+end
+
+function gadget:Initialize()
 end
