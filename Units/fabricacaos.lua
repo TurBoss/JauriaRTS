19a49bff0ade62fee270fa1bb5e1b22793c1068a
PerroTron <perrotronn@gmail.com>
2014-08-31 00:12:16 +0200

    balance caos

---
+++ Units/fabricacaos.lua
@@ -0,0 +1,66 @@
+local unitName = "fabricacaos"
+
+local unitDef = {
+	
+	-- Internal settings
+	
+	Category = "LAND",
+	ObjectName = "fabricacaos.dae",	
+	TEDClass = "PLANT",
+	script = "fabricacaos.lua",
+	buildPic = "placeholder.png",
+	
+	-- Unit limitations and properties
+	
+	Description = "a simple factory that builds mobile units",
+	MaxDamage = 20000,
+	Name = "Fabrica",
+	RadarDistance = 0,
+	SightDistance = 500,
+	Upright = 1,
+	levelground = 1,
+	
+	--cost
+	
+	buildCostMetal = 200,
+	buildCostEnergy = 0,
+	buildTime = 25,
+	
+	--economy
+	
+	EnergyStorage = 0,
+	EnergyUse = 0,
+	MetalStorage = 0,
+	EnergyMake = 0, 
+	MakesMetal = 0, 
+	MetalMake = 5,
+	
+	--Pathfinding and related
+	
+	FootprintX = 8,
+	FootprintZ = 8,
+	MaxSlope = 15,
+	YardMap =" oooooooo occcccco occcccco occcccco occcccco occcccco occcccco occcccco",
+	
+	-- Building
+	
+	Builder = true,
+	canMove = true,
+	Reclaimable = false,
+	ShowNanoSpray = true,
+	CanBeAssisted = false,
+	workerTime = 1,
+	
+	buildoptions = {
+		"adept",
+		"raider",
+		"marauder",
+		"putris",
+		"besieger",
+		"desolator",
+		"juggernaut",
+		"archaon",
+	},
+}
+
+return lowerkeys({ [unitName] = unitDef })
