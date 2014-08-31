a4149816058838a38c2be8a414890e25bd9bc80e
TurBoss <j.l.toledano.l@gmail.com>
2014-08-31 23:03:52 +0200

    fix Taurus

--- Scripts/taurus.lua
+++ Scripts/taurus.lua
@@ -14,8 +14,8 @@ local ruedasd = piece "ruedasd"
 
 local armad = piece "armad"
 local armai = piece "armai"
---local basearma = piece "basearma"
---local basearma = piece "basearmai"
+local basearmad = piece "basearmad"
+local basearmai = piece "basearmai"
 local flared = piece "flared"
 local flarei = piece "flarei"
 
@@ -26,26 +26,29 @@ local SIG_AIM2 = 2
 
 local smoke = SFX.CEG
 
+local active_barrel = 1		--the barrel that the next shot will be fired from
+local number_of_barrels = 2		--how many barrel there are in total
+
 function script.Create()
 	
 end
 
 ---AIMING & SHOOTING---
 function script.AimFromWeapon1() 
-	return armad
+	return chasis
 end
 
-function script.AimFromWeapon2() 
-	return armai
+function script.QueryWeapon1()
+	if (active_barrel == 1) then
+		--Spring.Echo ("torpedo1")
+		return flared
+	end
+	if (active_barrel == 2) then
+		--Spring.Echo ("torpedo2")
+		return flarei
+	end
 end
 
-function script.QueryWeapon1() 
-	return flared
-end
-
-function script.QueryWeapon2() 
-	return flarei
-end
 
 --must return true to allow the weapon to shot. return false denies the weapon from shooting
 --can be used delay the shooting until a "turn turret" animation is completed
@@ -55,27 +58,13 @@ function script.AimWeapon1( heading, pitch )
 	SetSignalMask(SIG_AIM1)
 	
 	Turn(cabinahierro, z_axis, heading, math.rad(80))
-	--Turn(basearma, x_axis, -pitch, math.rad(80))
+	Turn(basearmai, x_axis, -pitch, math.rad(80))
+	Turn(basearmad, x_axis, -pitch, math.rad(80))
 	
 	--wait until the weapon is pointed in the right direction
 	WaitForTurn (cabinahierro, z_axis)
-	--WaitForTurn (basearma, x_axis)
-
-	
-	return true
-end
-
-function script.AimWeapon2( heading, pitch )
-	--make sure the aiming animation is only run once
-	Signal(SIG_AIM2)
-	SetSignalMask(SIG_AIM2)
-	
-	Turn(cabinahierro, z_axis, heading, math.rad(80))
-	--Turn(basearma, x_axis, -pitch, math.rad(80))
-	
-	--wait until the weapon is pointed in the right direction
-	WaitForTurn (cabinahierro, z_axis)
-	--WaitForTurn (basearma, x_axis)
+	WaitForTurn (basearmai, x_axis)
+	WaitForTurn (basearmad, x_axis)
 
 	
 	return true
@@ -84,28 +73,32 @@ end
 --called after the weapon has fired
 function script.FireWeapon1()
 	
-	--EmitSfx(flare, smoke)
+	--switch to the next barrel:
+	active_barrel = active_barrel + 1
+	--if all barrels have fired, start the cyclus from the beginning:
+	if (active_barrel > number_of_barrels) then active_barrel = 1 end
 	
-	Move(armad, y_axis, 4)
-	Sleep(150)
-	Move(armad, y_axis, 0, 20)
+	if (active_barrel == 1) then
+		EmitSfx(flared, smoke)
+		Move(armad, y_axis, 4)
+		Sleep(150)
+		Move(armad, y_axis, 0, 20)
+	end
+	if (active_barrel == 2) then
+		EmitSfx(flarei, smoke)
+		Move(armai, y_axis, 4)
+		Sleep(150)
+		Move(armai, y_axis, 0, 20)
+	end
+	
 	
 end
 
-function script.FireWeapon2()
-	
-	--EmitSfx(flarei, smoke)
-	
-	Move(armai, y_axis, 4)
-	Sleep(150)
-	Move(armai, y_axis, 0, 20)
-	
-end
 
 function script.StartMoving(heading )
 	Turn (cabinahierro, z_axis, heading, math.rad(80))
-	Turn(armad, x_axis, 0, math.rad(80))
-	Turn(armai, x_axis, 0, math.rad(80))
+	Turn(basearmad, x_axis, 0, math.rad(80))
+	Turn(basearmai, x_axis, 0, math.rad(80))
 	Spin(ruedasa, x_axis, wheel_speed)
 	Spin(ruedasb, x_axis, wheel_speed)
 	Spin(ruedasc, x_axis, wheel_speed)
