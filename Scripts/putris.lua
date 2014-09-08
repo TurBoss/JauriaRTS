c2545706a4dc95849908a7c1d37f4f22d453c3ab
TurBoss <j.l.toledano.l@gmail.com>
2014-09-08 03:44:19 +0200

    putris model upgrade
    nueva jungla escorpion
    texturas marauder
    texturas putris
    texturas desolator
    fix fabricacaos model

--- Scripts/putris.lua
+++ Scripts/putris.lua
@@ -2,15 +2,15 @@ local base = piece('base');
 
 local flare = piece('flare');
 
-local mortero = piece('mortero');
+local bola = piece('bola');
 
-local ruedas = piece('ruedas');
+local canon = piece('canon');
 
-local adept_d = piece('adept_d');
-
-local adept_i = piece('adept_i');
+local cuello = piece('cuello');
 
 local root = piece('root');
+
+local basecuello = piece('basecuello');
 
 local SIG_AIM = 1
 
@@ -21,7 +21,7 @@ end
 
 ---AIMING & SHOOTING---
 function script.AimFromWeapon1() 
-	return mortero
+	return canon
 end
 
 function script.QueryWeapon1() 
@@ -33,28 +33,30 @@ function script.FireWeapon1()
 	EmitSfx(flare, smoke)
 end
 
-function script.AimWeapon1( heading )
+function script.AimWeapon1( heading, pitch )
 	--make sure the aiming animation is only run once
 	Signal(SIG_AIM)
 	SetSignalMask(SIG_AIM)
-	Turn(mortero, z_axis, heading, math.rad(280))
+	Turn(basecuello, z_axis, heading, math.rad(140))
+	Turn(cuello, x_axis, -pitch, math.rad(140))
 	--wait until the weapon is pointed in the right direction
-	WaitForTurn (mortero, z_axis)
+	WaitForTurn (basecuello, z_axis)
+	WaitForTurn (cuello, x_axis)
 	return true
 end
 
 function script.StartMoving(heading)
-	Turn (mortero, z_axis, heading, math.rad(80))
-	Spin(ruedas, x_axis, math.rad(180))
+	Turn(basecuello, z_axis, heading, math.rad(140))
+	Turn(cuello, x_axis, 0, math.rad(140))
 
 end
 
 function script.StopMoving()
-	StopSpin (ruedas,x_axis)
 end
 
 ----death animation
 function script.Killed(recentDamage, maxHealth)
 	Explode (base, SFX.SHATTER)
-	Explode (mortero, SFX.FIRE)
+	Explode (canon, SFX.FIRE)
+	Explode (cuello, SFX.SHATTER)
 end
