1b2a5e2779b83bd96242d4df142b2ee4fca31f02
TurBoss <j.l.toledano.l@gmail.com>
2014-09-11 04:05:19 +0200

    putris model upgrade

--- Scripts/putris.lua
+++ Scripts/putris.lua
@@ -2,15 +2,13 @@ local base = piece('base');
 
 local flare = piece('flare');
 
-local bola = piece('bola');
+local mortero = piece('mortero');
 
-local canon = piece('canon');
+local canon = piece('mortero');
 
-local cuello = piece('cuello');
+local peana = piece('peana');
 
 local root = piece('root');
-
-local basecuello = piece('basecuello');
 
 local SIG_AIM = 1
 
@@ -21,7 +19,7 @@ end
 
 ---AIMING & SHOOTING---
 function script.AimFromWeapon1() 
-	return canon
+	return mortero
 end
 
 function script.QueryWeapon1() 
@@ -30,24 +28,20 @@ end
 
 --called after the weapon has fired
 function script.FireWeapon1()
-	EmitSfx(flare, smoke)
 end
 
 function script.AimWeapon1( heading, pitch )
 	--make sure the aiming animation is only run once
 	Signal(SIG_AIM)
 	SetSignalMask(SIG_AIM)
-	Turn(basecuello, z_axis, heading, math.rad(140))
-	Turn(cuello, x_axis, -pitch, math.rad(140))
+	Turn(base, z_axis, heading, math.rad(140))
 	--wait until the weapon is pointed in the right direction
-	WaitForTurn (basecuello, z_axis)
-	WaitForTurn (cuello, x_axis)
+	WaitForTurn (base, z_axis)
 	return true
 end
 
 function script.StartMoving(heading)
-	Turn(basecuello, z_axis, heading, math.rad(140))
-	Turn(cuello, x_axis, 0, math.rad(140))
+	Turn(peana, z_axis, heading, math.rad(140))
 
 end
 
@@ -57,6 +51,5 @@ end
 ----death animation
 function script.Killed(recentDamage, maxHealth)
 	Explode (base, SFX.SHATTER)
-	Explode (canon, SFX.FIRE)
-	Explode (cuello, SFX.SHATTER)
+	Explode (mortero, SFX.FIRE)
 end
