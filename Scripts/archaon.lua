430785a65a95995834d63793b9d88407866b7179
PerroTron <perrotronn@gmail.com>
2014-09-16 02:43:52 +0200

    new archaon

--- Scripts/archaon.lua
+++ Scripts/archaon.lua
@@ -1,37 +1,48 @@
 local base = piece('base');
-local canon0 = piece('canon0');
-local canon1 = piece('canon1');
-local canon2 = piece('canon2');
-local canon3 = piece('canon3');
-local canon4 = piece('canon4');
-local canon5 = piece('canon5');
-local canon6 = piece('canon6');
-local flare0 = piece('flare0');
-local flare1 = piece('flare1');
-local flare2 = piece('flare2');
-local flare3 = piece('flare3');
-local flare4 = piece('flare4');
-local flare5 = piece('flare5');
-local flare6 = piece('flare6');
+local flare = piece('flare');
+local ojo = piece('ojo');
+local patas = piece('patas');
+local root = piece('root');
 
 
+--local wheel_speed = math.rad(180)
 
-
-local SIG_AIM1 = 1
-local SIG_AIM2 = 2
-local SIG_AIM3 = 4
-local SIG_AIM4 = 8
-local SIG_AIM5 = 16
-local SIG_AIM6 = 32
-local SIG_AIM0 = 64
-
+local SIG_AIM = 1
 
 function script.Create()
-    
+	
 end
-         
+
+---AIMING & SHOOTING---
+function script.AimFromWeapon1() 
+	return ojo
+end
+
+function script.QueryWeapon1() 
+	return flare
+end
+
+--must return true to allow the weapon to shot. return false denies the weapon from shooting
+--can be used delay the shooting until a "turn turret" animation is completed
+function script.AimWeapon1( heading, pitch )
+	--make sure the aiming animation is only run once
+	Signal(SIG_AIM)
+	SetSignalMask(SIG_AIM)
+	
+	Turn(base, z_axis, heading, math.rad(560))
+	
+	--wait until the weapon is pointed in the right direction
+	WaitForTurn (base, z_axis)
+	
+	return true
+end
+
+--called after the weapon has fired
+function script.FireWeapon1()
+end
 
 function script.StartMoving(heading)
+	Turn (base, z_axis, heading, math.rad(80))
 
 end
 
@@ -39,130 +50,13 @@ function script.StopMoving()
 
 end
 
-
----AIMING & SHOOTING---
-function script.AimFromWeapon1() 
-	return canon1
-end
-
-function script.AimFromWeapon2() 
-	return canon2
-end
-
-function script.AimFromWeapon3() 
-	return canon3
-end
-
-function script.AimFromWeapon4() 
-	return canon4
-end
-
-function script.AimFromWeapon5() 
-	return canon5
-end
-
-function script.AimFromWeapon6() 
-	return canon6
-end
-
-function script.AimFromWeapon7() 
-	return canon0
-end
-
----
-
-function script.QueryWeapon1() 
-	return flare1
-end
-
-function script.QueryWeapon2() 
-	return flare2
-end
-
-function script.QueryWeapon3() 
-	return flare3
-end
-
-function script.QueryWeapon4() 
-	return flare4
-end
-
-function script.QueryWeapon5() 
-	return flare5
-end
-
-function script.QueryWeapon6() 
-	return flare6
-end
-
-function script.QueryWeapon7() 
-	return flare0
-end
-
-
---called after the weapon has fired
---
-
-
-function script.FireWeapon1()
-end
-
-function script.FireWeapon2()
-end
-
-function script.FireWeapon3()
-	Move(canond, y_axis, 4)
-	Sleep(100)
-	Move(canond, y_axis, 0, 20)
-end
-
-function script.FireWeapon4()
-	Move(canoni, y_axis, 4)
-	Sleep(100)
-	Move(canoni, y_axis, 0, 20)
-end
-
-function script.AimWeapon1( heading, pitch )
-	--make sure the aiming animation is only run once
-	Signal(SIG_AIM)
-	SetSignalMask(SIG_AIM)
-	Turn(cabeza, z_axis, heading, math.rad(200))
-	Turn(canond, x_axis, -pitch, math.rad(200))
-
-	--wait until the weapon is pointed in the right direction
-	WaitForTurn (cabeza, z_axis)
-	WaitForTurn (canond, x_axis)
-	return true
-end
-
-function script.AimWeapon2( heading, pitch )
-	Turn(canoni, x_axis, -pitch, math.rad(200))
-	WaitForTurn (cabeza, z_axis)
-	WaitForTurn (canoni, x_axis)
-	return true
-end
-
-function script.AimWeapon3( heading, pitch )
-	--make sure the aiming animation is only run once
-	Turn(canond, x_axis, -pitch, math.rad(200))
-
-	--wait until the weapon is pointed in the right direction
-	WaitForTurn (cabeza, z_axis)
-	WaitForTurn (canond, x_axis)
-	return true
-end
-
-function script.AimWeapon4( heading, pitch )
-	Turn(canoni, x_axis, -pitch, math.rad(200))
-	WaitForTurn (cabeza, z_axis)
-	WaitForTurn (canoni, x_axis)
-	return true
-end
-
-----death animation
+----death animation: fall over & explode
 function script.Killed(recentDamage, maxHealth)
-	Explode (cabeza, SFX.SHATTER)
-	Explode (piernad, SFX.FIRE)
-	Explode (piernai, SFX.FIRE)
+
+	Explode (base, SFX.SHATTER)	
+	Explode (ojo, SFX.FIRE)
+	Explode (patas, SFX.FIRE)
+
+
 end
-            
+
---
+++ Weapons/weaponarchaon.lua
@@ -0,0 +1,57 @@
+
+local weaponName = "weaponarchaon"
+
+local weaponDef = {
+	
+	name                    = "Rayo Reaper",
+	weaponType              = "BeamLaser",
+	
+	Accuracy=20,
+	movingAccuracy=20,
+	
+	--damage
+	
+	avoidFeature = false,
+	
+	damage = {
+		default = 560,
+		heavyarmor = 510,
+		lightarmor = 485,
+		torrearmor = 420,
+	},
+--	areaOfEffect            = 10,
+	
+	--physics
+	
+	weaponVelocity          = 800,
+	reloadtime              = 0.1,
+	range                   = 420,
+--	sprayAngle              = 45,
+	weaponaceleration       = 300,
+	tolerance               = 18000,
+	lineOfSight             = true,
+	turret                  = true,
+	craterMult              = 0,
+--	burst                   = 2,
+--	burstrate               = 0.08,
+	--proyectiles             = 2,
+	
+	--apperance
+	
+	rgbColor                = [[1 0 0]],
+	size                    = 4.3,
+	stages                  = 1,
+	intensity               = 8,
+	separation              = 0.8,
+	thickness               = 9,
+	coreThickness           = 0.35,
+	laserFlareSize          = 10,
+	--largeBeamLaser          = true,
+	
+	--sounds
+	
+	soundStart              = "metralleta_gfx",
+	
+	collideFriendly=true,
+}
+return lowerkeys({[weaponName] = weaponDef})
