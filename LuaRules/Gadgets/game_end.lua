0977ab8c17d480637a3d35d738f3a9c4b5d476e9
TurBoss <j.l.toledano.l@gmail.com>
2014-12-30 18:19:20 +0100

    ctrl panel tests

--- LuaRules/Gadgets/game_end.lua
+++ LuaRules/Gadgets/game_end.lua
@@ -270,12 +270,11 @@ function gadget:RecvLuaMsg(msg)
 	local team = {}
 	if (msg:sub(1,4) == 'quit') then
 		team[0]  = msg:sub(5)
-		GameOver(team)
 		Spring.SendLuaUIMsg("gameover" .. team[0])
-		Spring.SendCommands("quit")
+		GameOver(team)
 	elseif (msg:sub(1,6) == 'resign') then
 		team[0] = msg:sub(7)
-		GameOver(team)
 		Spring.SendLuaUIMsg("gameover" .. team[0])
+		GameOver(team)
 	end
 end
