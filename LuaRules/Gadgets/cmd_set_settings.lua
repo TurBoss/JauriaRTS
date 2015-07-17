e4601bd544f306e6cb0f177ffdcc1319999f9385
TurBoss <j.l.toledano.l@gmail.com>
2015-07-17 05:06:23 +0200

    test: check default los state

--- LuaRules/Gadgets/cmd_set_settings.lua
+++ LuaRules/Gadgets/cmd_set_settings.lua
@@ -5,7 +5,7 @@ function gadget:GetInfo()
 		author    = "TurBoss",
 		date      = "14-7-2014",
 		license   = "GNU GPL v2 or later",
-		layer     = 1, 
+		layer     = 1,
 		enabled   = true  --  loaded by default?
 	}
 end
@@ -13,9 +13,9 @@ end
 function gadget:GamePreload()
 	--Spring.SendCommands("bind f4 showmetalmap")
 	Spring.SendCommands("bind d manualfire")
-	
-	Spring.SendCommands("togglelos")
-	Spring.SendCommands("toggleradarandjammer")
-	
+
+	--Spring.SendCommands("togglelos")
+	--Spring.SendCommands("toggleradarandjammer")
+
 	Spring.LoadCmdColorsConfig('move  0.5 1.0 0.5 0.0')
 end
