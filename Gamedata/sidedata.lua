77fd2fe51c4947c610fa6b13a39573e10a894c8a
TurBoss <j.l.toledano.l@gmail.com>
2018-04-06 01:23:51 +0200

    only leave one faction

--- Gamedata/sidedata.lua
+++ Gamedata/sidedata.lua
@@ -3,18 +3,6 @@ local sidedata = {
 	[1] = {
 		name = "Jauria",
 		startUnit = "fabricaporreton",
-	},
-	[2] = {
-		name = "Chaos",
-		startUnit = "fabricacaos",
-	},
-	[3] = {
-		name = "Europe",
-		startUnit = "fabricaeurope",
-	},
-	--[2] = {
-	--	name = "Heks",
-	--	startUnit = "fabricaheks",
-	--},
+	}
 }
 return sidedata
